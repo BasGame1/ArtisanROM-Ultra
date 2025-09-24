74bed284a75e72b22e735d59cdd0a5cc24e4336a
Android Artisan <romartisan2025@gmail.com>
2025-09-24 18:46:05 +0200

    unica: patches: miscs: customize.sh: update props to point as artisanrom

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -7,9 +7,9 @@ $ROM_IS_OFFICIAL || ROM_STATUS=" UNOFFICIAL"
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
 SET_PROP "system" "ro.build.display.id" "ArtisanROM $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($ROM_STATUS)"
 
-SET_PROP "system" "ro.extremerom.official" "$ROM_IS_OFFICIAL"
-SET_PROP "system" "ro.extremerom.version" "$ROM_VERSION"
-SET_PROP "system" "ro.extremerom.codename" "$ROM_CODENAME"
+SET_PROP "system" "ro.artisanrom.status" "$ROM_STATUS"
+SET_PROP "system" "ro.artisanrom.version" "$ROM_VERSION"
+SET_PROP "system" "ro.artisanrom.codename" "$ROM_CODENAME"
 
 # Disable FRP
 SET_PROP "vendor" "ro.frp.pst" ""
